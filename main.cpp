cc824a32914dd2be6d4a88be3e2d91ee898fa8e2
vipulgpt10 <vipulgpt10@gmail.com>
2015-12-25 17:52:47 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
+#include <iostream>     // make this code more reliable!!!!!!!!!!!!!!!!!
 #include "fibonacci.h"
 
 int main(int argc, char *argv[]){
  
+  printf("hello world");
   int num;
   std::cout << "Enter a Number: ";
   std::cin >> num;
 
   std::cout << fibonacci(num) << std::endl;
 }
+
+
